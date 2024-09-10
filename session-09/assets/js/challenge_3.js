eca3e68b788b0ea8605d76e63e4b06986fdaeb1b
Leif Bayley <20123841@tafe.wa.edu.au>
2024-09-10 10:54:12 +0800

    Modify practice js

--- session-09/assets/js/challenge_3.js
+++ session-09/assets/js/challenge_3.js
@@ -4,25 +4,28 @@ let numbers = [];
 
 for (let index = 0; index < 10; index++) {
     numbers.push(Math.random() * 100);
-    console.log(numbers);
 }
 
 // Calculate the sum of the numbers
 let sum = numbers.reduce((tally, current) => tally + current, 0);
-console.log(sum);
 // Calculate the average of the numbers
 let average = sum / numbers.length;
-console.log(average);
 
 // Calculate the square root of the average
 // Hint: use the inbuild Math object method()
+let squareRoot = Math.sqrt(average);
 
 // Calculate the largest number
+let max = Math.max(...numbers);
 
 // Calculate the factorial of the largest number
 // Hint: the factorial for 5 is 5*4*3*2*1
 let factorial = 1;
-for (let index = 1; index <= maxNumber; index++) {}
+for (let index = 1; index <= max; index++) {
+    factorial *= index;
+}
 
 // Display the results using template literals
 // Hint: log them in the console.
+let message = `Numbers: ${numbers}\n\nSum: ${sum}\nAverage: ${average}\nSqrt: ${squareRoot}\nMax: ${max}\nFactorial: ${factorial}`;
+console.log(message);
