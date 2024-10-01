12d56cd62ffcf59c4fa30485dc979d4e01f1f2cd
Leif Bayley <20123841@tafe.wa.edu.au>
2024-10-01 09:01:57 +0800

    Do week 11 challenges 1 and 2

--- session-11/assets/js_starter/scripts_challenge_2.js
+++ session-11/assets/js_starter/scripts_challenge_2.js
@@ -2,3 +2,14 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // Your code goes here
+// for (const [index, value] of numbers.entries()) {
+//     if (value % 2 == 0) {
+//         console.log(`${value} is even`);
+//         continue;
+//     }
+//     console.log(`${value} is odd`);
+// }
+for (const [index, value] of numbers.entries()) {
+    let type = value % 2 == 0 ? "even" : "odd";
+    console.log(`${value} is ${type}`);
+}
