93fe50e4c8586f73678754e9bf7d2ab1836e5f90
Leif Bayley <20123841@tafe.wa.edu.au>
2024-10-01 09:22:44 +0800

    Week 11 challenge 3

--- session-11/assets/js_starter/scripts_challenge_3.js
+++ session-11/assets/js_starter/scripts_challenge_3.js
@@ -1,10 +1,13 @@
 // Challenge 3 Starter
 const product = {
-  name: "Southern Spain",
-  location: "Valencia",
-  price: 100,
-  agent: "John",
-  bookingId: "25256A"
+    name: "Southern Spain",
+    location: "Valencia",
+    price: 100,
+    agent: "John",
+    bookingId: "25256A",
 };
 
 // Your code goes here
+for (let item in product) {
+    console.log(`${item}: ${product[item]}`);
+}
