fae919a289e1fe3106643c1abbb0c9ecd2f791af
Leif Bayley <20123841@tafe.wa.edu.au>
2024-10-01 11:06:16 +0800

    Week 11 challenges 4 and 5

--- session-11/assets/js_starter/scripts_challenge_4.js
+++ session-11/assets/js_starter/scripts_challenge_4.js
@@ -1,8 +1,13 @@
 // Challenge 4 Starter
-const eventLinks = document.querySelectorAll();
-
+const eventLinks = document.querySelectorAll("[data-event]");
+console.log(eventLinks);
 function logEvent(eventDetails) {
+    console.log(`${eventLinks[eventDetails].innerHTML.toLocaleUpperCase()}`);
     // Your code goes here
 }
 
 // Your code goes here
+for (const [eventDetails] of eventLinks.entries()) {
+    console.log(eventDetails);
+    logEvent(eventDetails);
+}
--- session-11/assets/js_starter/scripts_challenge_5.js
+++ session-11/assets/js_starter/scripts_challenge_5.js
@@ -2,10 +2,16 @@
 const sidebar = document.querySelector('.sidebar');
 
 function toggleSidebarContent() {
-  // Your code goes here
+    sidebar.children[1].removeChild(sidebar.children[1].firstChild);
+
+    const newElement = document.createElement("p");
+    newElement.textContent = "New content";
+    sidebar.appendChild(newElement);
 }
 
 // Add a button to toggle the sidebar content
-
-
+const toggleButton = document.createElement("button");
+toggleButton.textContent = "Click me";
+toggleButton.addEventListener("click", toggleSidebarContent);
 // Append the button to the body
+document.body.append(toggleButton);
