e652a385422bb97adad127c58edfe2ca72bc9beb
Leif Bayley <20123841@tafe.wa.edu.au>
2024-10-08 10:02:56 +0800

    feat: Week 12 challenges 1 and 2

--- session-12/assets/js_starter/scripts_challenge_1.js
+++ session-12/assets/js_starter/scripts_challenge_1.js
@@ -3,9 +3,15 @@
 let numbers = [1, 2, 3, 4, 5];
 
 // Use the pop() method to remove the last element from the array
+const removed_element = numbers.pop();
 
 // Use the unshift() method to add the last element to the beginning of the array
+numbers.unshift(removed_element);
 
 // Use the slice() method to exclude the first 3 elements and return a new array
+console.log(numbers);
+const new_arr = numbers.slice(0, 3);
 
 // Set the value of the outputfield inner html to the new array
+let elem = document.getElementById("OutputField");
+elem.innerHTML = new_arr;
--- session-12/assets/js_starter/scripts_challenge_2.js
+++ session-12/assets/js_starter/scripts_challenge_2.js
@@ -4,9 +4,14 @@ let array1 = [1, 2, 3, 2];
 let array2 = [4, 5, 6, 4];
 
 // Use the spread operator to merge the two arrays
+let array3 = [...array1, ...array2];
 
 // Use the Set object to remove duplicates
+let array_without_duplicates = [...new Set(array3)];
 
 // Use array destructuring to assign the first three elements to variables
+let [first, second, third] = array_without_duplicates;
 
 // Set the value of the output filed to be the second element of the unique array
+let elem = document.getElementById("OutputField");
+elem.innerHTML = array_without_duplicates;
